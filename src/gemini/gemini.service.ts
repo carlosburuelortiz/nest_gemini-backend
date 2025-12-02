e692afcad9aab99f65389266806f7ba98ebb2ac5
Carlos Buruel <charly.ob.26@gmail.com>
2025-12-02 13:38:40 -0600

    23. Caso de uso - BasicPrompt

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,20 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { BasicPromptDto } from './dtos/basic-prompt-dto';
 import { GoogleGenAI } from "@google/genai";
+import { basicPromptUseCase } from './use-cases/basic-prompt.use-cases';
 
 @Injectable()
 export class GeminiService {
     private ai = new GoogleGenAI({});
 
     async basicPrompt(basicPromptDto: BasicPromptDto) {
-        const response = await this.ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: basicPromptDto.prompt,
-            config: {
-                systemInstruction: 'Responde únicamente en español, en formato markdown. Usa negritas de esta forma __',
-            }
-        });
-
-        return response.text;
+        return basicPromptUseCase(this.ai, basicPromptDto);
     }
 }
