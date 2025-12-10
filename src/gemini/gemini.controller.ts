9f7675b881cc455827dfa4630fc7e6a4f750202a
Carlos Buruel <charly.ob.26@gmail.com>
2025-12-10 16:43:31 -0600

    60. Conversaciones de varios turnos

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -3,10 +3,29 @@ import { GeminiService } from './gemini.service';
 import { BasicPromptDto } from './dtos/basic-prompt-dto';
 import type { Response } from 'express';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { ChatPromptDto } from './dtos/chat-prompt-dto';
+import { GenerateContentResponse } from '@google/genai';
 
 @Controller('gemini')
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
+
+  async outputStreamResponse(
+    res: Response,
+    stream: AsyncGenerator<GenerateContentResponse, any, any>
+  ) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(HttpStatus.OK);
+
+    let resultText = '';
+    for await(const chunk of stream) {
+      const piece = chunk.text;
+      resultText += piece;
+      res.write(piece);
+    }
+    res.end();
+    return resultText;
+  }
 
   @Post('basic-prompt')
   basicPrompt(@Body() basicPromptDto: BasicPromptDto) {
@@ -24,14 +43,22 @@ export class GeminiController {
 
     const stream = await this.geminiService.basicPromptStream(basicPromptDto);
 
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(HttpStatus.OK);
+    void this.outputStreamResponse(res, stream);
+  }
 
-    for await(const chunk of stream) {
-      const piece = chunk.text;
-      console.log(piece);
-      res.write(piece);
-    }
-    res.end();
+  @Post('chat-stream')
+  @UseInterceptors(FilesInterceptor('files'))
+  async chatStream(
+    @Body() chatPromptDto: ChatPromptDto,
+    @Res() res: Response,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    chatPromptDto.files = files;
+
+    const stream = await this.geminiService.basicPromptStream(chatPromptDto);
+
+    const data = await this.outputStreamResponse(res, stream);
+
+    console.log({data});
   }
 }
