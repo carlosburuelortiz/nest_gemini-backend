d7e80411f83f1a91799eeb87f30a9379547d55a1
Carlos Buruel <charly.ob.26@gmail.com>
2025-12-22 13:58:49 -0600

    78. Generación de imágenes - Caso de uso

--- src/gemini/use-cases/image-generation.use-case.ts
+++ src/gemini/use-cases/image-generation.use-case.ts
@@ -1,6 +1,7 @@
-import { createPartFromUri, GoogleGenAI } from "@google/genai";
+import { ContentListUnion, createPartFromUri, GoogleGenAI } from "@google/genai";
 import { geminiUploadFiles } from "../helpers/gemini-upload-files";
 import { ImageGenerationDto } from "../dtos/image-generation-dto";
+import { v4 as uuidV4 } from 'uuid'
 
 interface Options {
     model?: string;
@@ -18,17 +19,46 @@ export const imageGenerationUseCase = async (
     options?: Options,
 ): Promise<ImageGenerationResponse> => {
     const { prompt, files = [] } = imageGenerationDto;
+    const contents: ContentListUnion = [{ text: prompt }]
+
     const uploadedFiles = await geminiUploadFiles(ai, files);
+    uploadedFiles.forEach(file => {
+        contents.push(createPartFromUri(file.uri ?? '', file.mimeType ?? ''));
+    });
 
     const {
-        model = 'gemini-2.5-flash',
-        systemInstruction = `
-        Responde únicamente en español
-        En formato markdown
-        Usa negritas de esta forma __
-        Usa el sistema métrico decimal
-    `,
+        model = 'gemini-2.5-flash-image',
     } = options ?? {};
+
+    const response = await ai.models.generateContent({
+        model: model,
+        contents: contents,
+        config: {
+            responseModalities: ['TEXT', 'IMAGE'],
+        },
+    });
+
+    console.log(response);
+
+    let imageUrl = '';
+    let text = '';
+    const imageId = uuidV4();
+
+    for (const part of response.candidates?.[0]?.content?.parts ?? []) {
+        if (part.text) {
+            text = part.text;
+            continue;
+        }
+        if (!part.inlineData) {
+            continue;
+        }
+
+        const imageData = part.inlineData.data!;
+        const buffer = Buffer.from(imageData, 'base64');
+        console.log(buffer);
+    }
+
+    console.log(text);
 
     return {
         imageUrl: 'xyz',
